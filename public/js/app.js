4278ece26426dde531c64f3dee4e351a921caafd
olkhon <alexander.stapel@gmail.com>
2020-12-08 17:34:16 +0100

    add eventPrevent Default

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,11 @@
+// listen for submit event
 
+let addSubmit = document.getElementById('add');
+
+const addToDo = (event) => {
+    event.preventDefault();
+}
+
+addSubmit.addEventListener('click', addToDo);
+
+//
