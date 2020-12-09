621119e981d66b8e3461f6cbca05d1b3bdbd0e5b
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2020-12-09 12:04:33 +0100

    add event for toggle done/todo

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,22 @@
+// to-do list
+const toDoList = document.getElementsByTagName('ul')[0];
 
+// listen for submit event
+
+let addSubmit = document.getElementById('add');
+
+const addToDo = (event) => {
+    event.preventDefault();
+}
+
+addSubmit.addEventListener('click', addToDo);
+
+// eventHandler for toggle to-do to be striked out when done or unstrike
+const toggleDone = (e) => {
+/*   console.log(this);
+  console.log(e.target); */
+  e.target.classList.toggle('strike');
+}
+
+// eventListener for toggeling done/todo items
+toDoList.addEventListener('click', toggleDone);
