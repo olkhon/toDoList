9e397542fa4cf5027ccd484de1abc4d6597871b3
Hermann Marx <marxhermann@web.de>
2020-12-10 12:21:14 +0100

    Added the creation of a deleteButton when an todo is being created.

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,14 @@
 // to-do list
-const toDoList = document.getElementById('itemsList');
+const toDoList = document.getElementById("itemsList");
 
 // listen for submit event
-let addSubmit = document.getElementById('add');
+let addSubmit = document.getElementById("add");
 
 const addToDo = (event) => {
-    event.preventDefault();
-}
+  event.preventDefault();
+};
 
-addSubmit.addEventListener('click', addToDo);
+addSubmit.addEventListener("click", addToDo);
 
 //
 
@@ -16,11 +16,11 @@ addSubmit.addEventListener('click', addToDo);
 
 const deleteButton = document.getElementsByTagName("button");
 
-for (let i = 0; i < deleteButton.length; i++){
-    deleteButton[i].addEventListener("click", function () {
-        const toDoToDelete = document.getElementsByTagName("li");
-        toDoToDelete[i].style.display = "none";
-    })
+for (let i = 0; i < deleteButton.length; i++) {
+  deleteButton[i].addEventListener("click", function () {
+    const toDoToDelete = document.getElementsByTagName("li");
+    toDoToDelete[i].style.display = "none";
+  });
 }
 
 //
@@ -30,28 +30,37 @@ const toggleDone = (event) => {
   console.log(event.target);
   /* console.log(this);
   console.log(event.target); */
-  event.target.classList.toggle('strike');
+  event.target.classList.toggle("strike");
   event.target.parentNode.appendChild(event.target);
-}
+};
 
 // eventListener for toggeling done/todo items
-toDoList.addEventListener('click', toggleDone);
+toDoList.addEventListener("click", toggleDone);
 
 // add new Item to do list
 
-let listItems = document.getElementById('itemsList');
+let listItems = document.getElementById("itemsList");
 
+addSubmit.addEventListener("click", () => {
+  let getInput = document.getElementById("inputUser").value;
 
-addSubmit.addEventListener('click', () => {
-
-    let getInput = document.getElementById('inputUser').value;
-
-    if (getInput !== "") {
-        let newLi = document.createElement('LI');
-        let textInput = document.createTextNode(getInput);
-        newLi.appendChild(textInput);
-        listItems.appendChild(newLi);
-    } else {
-      alert('Input cannot be empty');
-    }
+  if (getInput !== "") {
+    //Create LI
+    let newLi = document.createElement("LI");
+    let textInput = document.createTextNode(getInput);
+    newLi.appendChild(textInput);
+    //Create Button
+    let newButton = document.createElement("button");
+    let buttonText = document.createTextNode("X");
+    newButton.appendChild(buttonText);
+    newButton.addEventListener("click", function () {
+      newLi.remove();
+    });
+    //Create full ToDo (LI + Button)
+    newLi.appendChild(newButton);
+    //Append new List Item (newLi)
+    listItems.appendChild(newLi);
+  } else {
+    alert("Input cannot be empty");
+  }
 });
