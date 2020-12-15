190babf57b54190279b0287241a1c07e6223eb71
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2020-12-15 18:17:24 +0100

    create class ToDoList

--- public/js/app.js
+++ public/js/app.js
@@ -14,3 +14,30 @@ class ToDo {
 
   }
 }
+
+class ToDoList {
+  constructor (toDos = []) {
+    this._toDos = toDos;
+    this._toDoList = createElement('UL').id('itemsList');
+    console.log(this._toDoList);
+    document.forms[0].append(this._toDoList);
+
+  }
+
+  /* execute when click the add button, gets input value, does trim, validation ... , creates a new ToDo, adds it to the List (call addToList() method)*/
+  createToDo (inputValue) {
+
+  }
+
+  /* adds the ToDo to the List (appendChild)*/
+  addToList () {
+
+  }
+
+  /* remove() called on html elements -> target is the button remove the parent (li) */
+  deleteToDo () {
+    
+  }
+
+
+}
