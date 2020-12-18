b785332cb84f060a3f66751068ae69a22b65ab8a
olkhon <alexander.stapel@gmail.com>
2020-12-18 14:14:43 +0100

    delete logic

--- public/js/app.js
+++ public/js/app.js
@@ -1,80 +1,83 @@
-
-
-
 class ToDo {
-  constructor (value) {
-    this._toDo = "<li>myTodo</li>";
-    this._markedAsDone = false;
-  }
+  constructor(value) {
+    this._textInput = document.createTextNode(value);
+    this._newLi = document.createElement("LI");
+    this._newLi.appendChild(this._textInput);
 
-  /* click on the ToDo, adding/removing class strike */
-  toggleDone () {
+    this._newLi.id = Date.now();
 
-  }
-
-  /* handles Click on the delete Button */
-  deleteToDo () {
-
+    this._newButton = document.createElement("button");
+    this._buttonText = document.createTextNode("X");
+    this._newButton.appendChild(this._buttonText);
+    this._newLi.appendChild(this._newButton);
   }
 }
 
 class ToDoList {
-  constructor () {
+  constructor() {
     this._toDos = [];
 
+
     // we ask aria, ? set/function : ok?
-    this._toDoList = document.createElement('UL');
+    this._toDoList = document.createElement("UL");
     this._toDoList.id = "itemsList";
     //console.log(this._toDoList);
-    document.querySelector('main').append(this._toDoList);
-  }
-
-
-
-
-
-
-  /* execute when click the add button, gets input value, does trim, validation ... , creates a new ToDo, adds it to the List (call addToList() method)*/
-  createToDo (inputValue) {
-    console.log('to do vreated')
+    document.querySelector("main").append(this._toDoList);
   }
 
   /* adds the ToDo to the List (appendChild)*/
-  addToList () {
+  addToList() {
+    this._toDos.push(this._toDoObject);
+  }
 
+  /* execute when click the add button, gets input value, does trim, validation ... , creates a new ToDo, adds it to the List (call addToList() method)*/
+  createToDo(inputValue) {
+    this._toDoObject = new ToDo(inputValue);
+    this._toDoList.appendChild(this._toDoObject._newLi);
+
+    this._toDoObject._newButton.addEventListener("click", this.deleteToDo.bind(this));
+    this._toDoObject._newLi.addEventListener("click", this.toggleDone.bind(this));
+
+    this.addToList(this._toDoObject);
+
+  }
+
+  /* click on the ToDo, adding/removing class strike */
+  toggleDone(event) {
+    console.log(this._toDos);
+    event.target.classList.toggle("strike");
+    event.target.parentNode.appendChild(event.target);
   }
 
   /* remove() called on html elements -> target is the button remove the parent (li) */
-  deleteToDo () {
+  deleteToDo(event) {
+
+    this._toDos.forEach((element, index) => {
+      if(element._newLi.id === event.target.parentElement.id) {
+        this._toDos.splice(index, 1);
+      }
+    });
+    event.target.parentElement.remove();
+
 
   }
-
-
 }
-
-
-
 
 const handleOnLoad = () => {
-    const myList = new ToDoList ();
-    return myList;
-}
+  return (myList = new ToDoList());
+};
 
-console.log(myList);
+//console.log(myList);
 
 const handleSubmit = (event) => {
-
-    event.preventDefault();
-
-    myList.createToDo(document.getElementById("inputUser").value);
-
-}
+  event.preventDefault();
+  myList.createToDo(document.getElementById("inputUser").value);
+};
 
 // initialization onload
 
-window.addEventListener('load', handleOnLoad);
+window.addEventListener("load", handleOnLoad);
 
 // submit Handler
 
-document.forms[0].addEventListener('submit', handleSubmit);
-
+document.forms[0].addEventListener("submit", handleSubmit);
